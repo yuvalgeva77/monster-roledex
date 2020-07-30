645debd4fd56d05da5ff17ea30620ce487d04758
yuvalgeva77 <yuvalgeva77@gmail.com>
2020-07-31 00:23:58 +0300

    Updates

---
+++ precache-manifest.a87c0dfb63096607d0c3ef629421f53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fc594b0cdb0595dbb9eab793f76d30",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "731803b97836d912d356",
+    "url": "/monster-roledex/static/css/main.74c015e7.chunk.css"
+  },
+  {
+    "revision": "30ff922f61e75086b083",
+    "url": "/monster-roledex/static/js/2.ff504122.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.ff504122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731803b97836d912d356",
+    "url": "/monster-roledex/static/js/main.0dbdc961.chunk.js"
+  },
+  {
+    "revision": "e6d61643876c45e1092b",
+    "url": "/monster-roledex/static/js/runtime-main.b7aefc9a.js"
+  }
+]);
